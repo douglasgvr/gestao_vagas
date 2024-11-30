ed02262c4fc876252f3e811139dc63f59e3a1978
douglasgvr <douglasfarmaangra@gmail.com>
2024-11-30 15:16:08 -0300

    criando a entidade das vagas (JobEntity) e ralacionando com a empresa atraves do @maytoone

---
+++ src/main/java/br/com/douglascoelho/gestao_vagas/modules/company/entities/JobEntity.java
@@ -0,0 +1,38 @@
+package br.com.douglascoelho.gestao_vagas.modules.company.entities;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+
+@Entity(name = "Job")
+@Data
+public class JobEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String description;
+    private String benefits;
+    private String level;
+
+    @ManyToOne() // varias vagas para uma compania
+    @JoinColumn(name = "company_id")
+    private CompanyEntity companyentity;
+
+    @Column(name = "company_id")
+    private UUID companyId;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+}
